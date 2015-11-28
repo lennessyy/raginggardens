f72dcec17ecee9b0d2527a16b0a64676c43d508f
Petar Petrov <petar.petrov.georgiev@gmail.com>
2015-11-28 16:28:24 +0100

    Fix build to use version from manifest.json

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 PWD=$(pwd)
 YUI=$(pwd)/yuicompressor-2.4.8pre.jar
 BUILD=$(pwd)/build
+VERSION=$(cat $PWD/manifest.json | grep \"version\" | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d ' ')
 
 if [ ! -d $BUILD ]; then
 	echo "Build folder($BUILD) does not exist!"
@@ -49,6 +50,8 @@ java -jar $YUI  -o '.js$:.js' *.js
 cd $BUILD
 sed -i 's/style.css/style.min.css/g' index.html
 sed -i 's/dev/prod/g' index.html
+sed -i "s/\:ver/:'$VERSION'/g" index.html
+sed -i "s/\+ver/+'$VERSION'/g" index.html
 sed -i '/<\!\-\-URCHIN\-\->/{
     s/<\!\-\-URCHIN\-\->//g
     r ../urchin
